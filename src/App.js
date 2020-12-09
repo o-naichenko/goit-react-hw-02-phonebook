ae4be5aba15a0a0df4af52e89c1d54da650f65c9
o-naichenko <o.naichenko@gmail.com>
2020-12-09 18:35:28 +0200

    create-react-app + deploy script

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+  
+  render() 
+
+
+  
+}
+
+export default App;
