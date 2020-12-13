a4697fcf848fd8246bdde5dc31b33a557963a12a
o-naichenko <o.naichenko@gmail.com>
2020-12-14 00:20:53 +0200

    Виправляє помилки, додає нормалізацію стилів
    Випрявляє помилку (значення фільтра не приводилось до нижнього регістру, як імена контактив, тому при введенні літер верхнього регістру фільтр не працював)

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import ContactList from './components/Contact-list';
 class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -42,7 +42,7 @@ class App extends Component {
       return this.state.contacts;
     } else {
       return this.state.contacts.filter(contact =>
-        contact.name.toLowerCase().includes(this.state.filter),
+        contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
       );
     }
   };
